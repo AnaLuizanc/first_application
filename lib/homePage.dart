2536cb72039814b3556e98379d8bb1a9053f0300
AnaLuizanc <analuizanc13@gmail.com>
2025-10-16 19:50:12 -0300

    feat: add content to pages and implement bottom navigation
    - Implements the logic for the `BottomNavigationBar` to allow switching between different pages.
    - Adds placeholder content for the first and second pages, including icons, titles, and descriptive text.
    - Sets the body of the `Scaffold` to display the currently selected page.

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -8,6 +8,88 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _pages = <Widget>[
+    // Página inicial
+    Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            Icons.home_work_outlined,
+            size: 100,
+            color: Color(0xFF4A7FB2),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Bem-vindo(a) à Tela Inicial!',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    ),
+    // Conteúdo da Página 1
+    Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            Icons.insert_emoticon,
+            size: 100,
+            color: Color(0xFF4A7FB2),
+          ),
+          Text(
+            'Conteúdo Qualquer',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: Text(
+              'Este é um espaço reservado para o conteúdo da primeira página. Aqui você pode adicionar widgets, textos, imagens e outras informações relevantes.',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+        ],
+      ),
+    ),
+    // Conteúdo da Página 2
+    Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            Icons.airplanemode_active,
+            size: 100,
+            color: Color(0xFF4A7FB2),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Página de Viagens',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: Text(
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+        ],
+      ),
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,23 +97,7 @@ class _HomepageState extends State<Homepage> {
         backgroundColor: Color(0xFF4A7FB2),
         title: const Text('Tela Inicial'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Icon(
-              Icons.home_work_outlined,
-              size: 100,
-              color: Color(0xFF4A7FB2),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Bem-vindo(a) à Tela Inicial!',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
-      ),
+      body: _pages[_selectedIndex],
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
@@ -69,7 +135,9 @@ class _HomepageState extends State<Homepage> {
         type: BottomNavigationBarType.fixed,
         backgroundColor: Color(0xFFA3BED9),
         selectedItemColor: Colors.blueAccent,
-        items:[
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: 'Home',
